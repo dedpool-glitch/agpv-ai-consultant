1f775679068c90b246998408ee33004be34c24d2
dedpool-glitch <nalinrajput2001@gmail.com>
2026-08-26 21:38:44 -0400

    Load default panel specs from panel_specs.json instead of hardcoding them

--- questionnaire/to_pvmaps.py
+++ questionnaire/to_pvmaps.py
@@ -4,17 +4,7 @@ from models.pvmaps.input_builder import create_default_pvmaps_input
 
 def build_pvmaps_input_from_questionnaire(state, lat, lon):
     panel_model = state["panel_model"]
-
-    if panel_model == "default values":
-        panel_specs = {
-            "cell_tech": "AL_BSF",
-            "module_height": 4.8,
-            "stc_eff_direct": 21.8,
-            "stc_eff_diffuse": 21.8,
-            "tcoeff": 0.0041,
-        }
-    else:
-        panel_specs = get_panel_specs(panel_model)
+    panel_specs = get_panel_specs(panel_model)
 
     return create_default_pvmaps_input(
         lat=lat,
